d941b1d7ca118fe910cb85d53616e65b7f06809d
soulSysAdm <ko.sys.adm@gmail.com>
2025-04-06 21:07:39 +0300

    Fixed 5

--- dist/api/telegramHandler.js
+++ dist/api/telegramHandler.js
@@ -1,14 +1,24 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = telegramHandler;
-// : Promise<VercelResponse>
-async function telegramHandler(req, res) {
-    try {
-        console.log('Telegram Handler started 1');
-        res.status(200).send('ok');
-    }
-    catch (error) {
-        console.error('❌ Ошибка основного webhook:', error);
-        res.status(500).send('Ошибка сервера');
-    }
+function telegramHandler(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            console.log('Telegram Handler started 1');
+            res.status(200).send('ok');
+        }
+        catch (error) {
+            console.error('❌ Ошибка основного webhook:', error);
+            res.status(500).send('Ошибка сервера');
+        }
+    });
 }
