f72820cd79baf0ab14ad7b82fab473e902e2fb05
soulSysAdm <ko.sys.adm@gmail.com>
2025-05-25 15:51:02 +0300

    Fixed allowed isers

--- dist/google/sheets/repeatSheet.js
+++ dist/google/sheets/repeatSheet.js
@@ -3,12 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.repeatSheet = repeatSheet;
 const index_1 = require("../index");
 const telegram_1 = require("../../telegram");
-const globals_1 = require("../../globals");
+let ALLOWED_USERS;
+if (process.env.VERCEL) {
+    ALLOWED_USERS = JSON.parse(process.env.ALLOWED_USERS || '[]');
+}
+else {
+    const dotenv = require('dotenv');
+    dotenv.config();
+    ALLOWED_USERS = JSON.parse(process.env.ALLOWED_USERS || '[]');
+}
 async function repeatSheet() {
     try {
         const sheetData = await (0, index_1.readSheet)();
         const { validMessages, unValidMessages } = (0, index_1.getTelegramMessages)(sheetData);
-        for (const chatId of globals_1.allowedUsers) {
+        const allowedUsersIds = ALLOWED_USERS.map((user) => user.id);
+        for (const chatId of allowedUsersIds) {
             for (const message of unValidMessages) {
                 await (0, telegram_1.sendTelegramMessage)(chatId, message);
             }
--- src/google/sheets/repeatSheet.ts
+++ src/google/sheets/repeatSheet.ts
@@ -1,13 +1,23 @@
 import { getTelegramMessages, readSheet } from '../index'
 import { sendTelegramMessage } from '../../telegram'
-import { allowedUsers } from '../../globals'
+import type { AllowedUser } from '../../types'
+
+let ALLOWED_USERS: AllowedUser[]
+
+if (process.env.VERCEL) {
+  ALLOWED_USERS = JSON.parse(process.env.ALLOWED_USERS || '[]')
+} else {
+  const dotenv = require('dotenv')
+  dotenv.config()
+  ALLOWED_USERS = JSON.parse(process.env.ALLOWED_USERS || '[]')
+}
 
 export async function repeatSheet() {
   try {
     const sheetData = await readSheet()
     const { validMessages, unValidMessages } = getTelegramMessages(sheetData)
-
-    for (const chatId of allowedUsers) {
+    const allowedUsersIds = ALLOWED_USERS.map((user) => user.id)
+    for (const chatId of allowedUsersIds) {
       for (const message of unValidMessages) {
         await sendTelegramMessage(chatId, message)
       }
